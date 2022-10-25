0523e9b8747ec0c71c07c277ffdb7e4913bfd224
LEGRAND Dylan (SNCF VOYAGEURS / DIRECTION DE LIGNE ALSACE / UO TRACTION DL ALSACE) <9919157t@commun.ad.sncf.fr>
2022-10-25 17:23:10 +0200

    quest 5 finished

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  model: User = new User("", "", "", "");
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log(this.model);
+}
+}
+
+export class User {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+
+  constructor(email: string, firstname: string, lastname: string, password: string) {
+      this.email = email;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.password = password;
+  }
 }
